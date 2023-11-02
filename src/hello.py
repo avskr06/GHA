5329bafef8373f46d394c95ba64f99f9232bdbd6
sree <144780588+avskr06@users.noreply.github.com>
2023-11-02 17:24:21 +0530

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -1,4 +1,4 @@
-```python
+python
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
@@ -6,4 +6,4 @@ def hello():
 return "Hello, World!"
 if __name__ == '__main__':
 app.run(debug=True, host='0.0.0.0')
-```
+
